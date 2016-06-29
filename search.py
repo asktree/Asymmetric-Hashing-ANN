258951980f2e7ba7abc3b71e8f07feaf9f1cc37e
Agrippa Kellum <ask252@cornell.edu>
2016-06-29 13:44:03 -0500

    reorder added

--- search.py
+++ search.py
@@ -15,6 +15,7 @@ def approximate_search(query, data, centroids, hash, R= 100):
     lookup = np.empty(tuple([num_chunks] + [num_centroids]))
 
     for i, chunk in enumerate(centroids):
+        # [optimization] parallelize this
         for j, centroid in enumerate(chunk):
             lookup[i, j] = scp.spatial.distance.sqeuclidean(query, centroid)
 
@@ -24,6 +25,21 @@ def approximate_search(query, data, centroids, hash, R= 100):
         approx_dist = 0
         for i, j in enumerate(h_i):
             approx_dist += lookup[i, j]
+            # [optimization] early out:
+            # if approx_dist >= approx_dists[0]:
+            #     break
         if approx_dist < approx_dists[0]:
             heapq._heapreplace_max(approx_dists, (approx_dist, index))
     return approx_dists
+
+
+def exact_reorder(query, data, candidates):
+    # should probably just delete the first axis of candidates
+    exact = []
+    # [optimization] parallelize this
+    for approx_dist, index in candidates:
+        exact_dist = scp.spatial.distance.sqeuclidean(query, data[index])
+        exact.append((exact_dist, index))
+
+    reorder = np.array(exact).sort(axis=0, kind='heapsort')
+    return reorder
