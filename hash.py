701a680408455874d832e7cbb9037a77881275c7
Wizzeh <me@wizzeh.com>
2016-08-05 17:37:47 -0700

    Rewrote for clarity.

--- hash.py
+++ hash.py
@@ -1,24 +1,25 @@
 import numpy as np 
-from numpy import transpose
 from scipy.cluster.vq import vq, kmeans2, whiten
-from scipy.optimize import leastsq
-import math
 
-def generate_hash_dict(to_hash, chunk_dim, save_to):
-    #to_hash: R^(N x M); save_to: string; chunk_dim: int =/= 0
-    to_hash = transpose(to_hash)
-    N = to_hash.shape[0]
-    M = to_hash.shape[1]
+def generate_hash_dict(to_hash, chunk_dim, minit='random'):
+    #to_hash: m x n matrix w/ m observations
 
-    #perform k-means on each chunk_dim chunk to find centroids
-    num_chunks = math.ceil(N/chunk_dim)
-    centroids = np.empty((num_chunks,256,N))
-    closest_centroids = np.empty((num_chunks,M),int)
-    for chunk in range(0,num_chunks):
-        whitened = whiten(transpose(to_hash[chunk_dim*chunk:chunk_dim*(chunk+1),:]))
-        out = kmeans2(whitened,256)
-        centroids[chunk,:,:] = out[0]
-        closest_centroids[chunk,:] = out[1]
+    M = to_hash.shape[0]
+    N = to_hash.shape[1]
 
-    #switch to spock's output spec
-    return (transpose(closest_centroids),centroids)
+    if chunk_dim <= 0:
+        raise ValueError("Chunk dimension must be strictly positive.")
+    if chunk_dim > N or N % chunk_dim != 0:
+        raise ValueError("Chunk dimension must divide N.")
+
+    #Create empty arrays for out output. Labels are stored as bytes.
+    #Slightly weird dimensionality due to spec
+    centroids = np.zeros((N/chunk_dim, 256, chunk_dim))
+    labels = np.zeros((M,N/chunk_dim),np.dtype('b'))
+
+    #Perform k-means on each chunk to find centroids.
+    for chunk in range(0, int(N/chunk_dim)):
+        centroids[chunk,:,:], labels[:,chunk] = kmeans2(whiten(
+            to_hash[:,chunk_dim*chunk:chunk_dim*(chunk+1)]),256,minit=minit)
+
+    return (labels,centroids)
